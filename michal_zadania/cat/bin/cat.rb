9325795da8c1be0b2ffae40b443ed8da4fd8ea96
Paweł Zwoliński <p.zwolin@gmail.com>
2016-06-22 18:10:04 +0200

    Additions to Michal CTO exercises
    Some fixes and additions/change requests for exercises
    assigned to me by Michal.
    
    Among others:
      * use File.open in a block syntax
      * read files sequentially
      * add regexp feature in grep.rb
      * option parser implemented
      * add calculator feature to RPN (ONP) parser

--- michal_zadania/cat/bin/cat.rb
+++ michal_zadania/cat/bin/cat.rb
@@ -1,12 +1,31 @@
 #!/usr/bin/env ruby
 
-unless filename = ARGV.first
-  puts "You need to pass name of the file"
+require 'optparse'
+
+def colorize text, color_code
+  "\e[#{color_code}m#{text}\e[0m"
+end
+
+options = {}
+
+OptionParser::new do |opts|
+  opts.banner = <<-bannertext
+
+Ruby implementation of unix cat command
+Usage: cat.rb file1 file2 file3...
+
+                bannertext
+end.parse!
+
+if ARGV.empty?
+  puts colorize("Error: You need to pass name of the file",31)
   exit 0
+else
+  ARGV.each do |filename|
+    File::open filename do |file|
+      file.each do |line|
+        print line
+      end
+    end
+  end
 end
-
-
-File::open filename do |file|
-  print file.read
-end
-
